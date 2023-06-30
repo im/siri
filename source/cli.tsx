48f06103fd13487ba09a955c758694255fb26ec6
tangxiaomi <81195314@qq.com>
2023-06-30 16:18:52 +0800

    update

---
+++ source/cli.tsx
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './app.js';
+
+const cli = meow(
+	`
+	Usage
+	  $ siri
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ siri --name=Jane
+	  Hello, Jane
+`,
+	{
+		importMeta: import.meta,
+		flags: {
+			name: {
+				type: 'string',
+			},
+		},
+	},
+);
+render(<App name={cli.flags.name} />);
