2310ba7156198fd06b65dbd48651df6060e8de6c
tangxiaomi <81195314@qq.com>
2023-06-30 18:10:37 +0800

    update

--- source/app.tsx
+++ source/app.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {Text} from 'ink';
 import OpenUrl from './openUrl.js';
+import OpenServe from './openServe.js';
 import { getConfig } from './config.js'
 import { Props } from './types.js'
 
 export default function App({cli}: Props) {
 	const { input  } = cli
 	const config = getConfig()
+
+
+	if (input.length && input[0] === 'serve') {
+		return OpenServe(cli, config)
+	}
 
 	if (input.length) {
 		return OpenUrl(cli, config)
--- source/openUrl.tsx
+++ source/openUrl.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import {Text} from 'ink';
+import { Text, Box } from 'ink';
 import { Config, Cli } from './types.js'
+import { Alert, } from '@inkjs/ui';
+import open from 'open'
 
 
-export default function OpenUrl(cli:Cli, config:Config) {
-	const { input,   } = cli
+export default function OpenUrl(cli: Cli, config: Config) {
+    const { input, } = cli
 
-	const name = input[0]
-	const url =  config.url[name]
+    const name = input[0]
+    const url = config.url[name] || ''
+    if (!url) {
+        return (
+            <Box width={60}>
+                <Alert variant="error"> <Text>请在配置文件<Text color="green"> .sirirc.json </Text> 中添加要打开的地址</Text></Alert>
+            </Box>
+        );
+    }
 
-	return (
-		<Text>
-			打开,{ name }: <Text color="green">{url}</Text>
-		</Text>
-	);
+    open(url)
+
+    return (
+        <Box width={60}>
+            <Alert variant="success"> 打开 {name}: <Text color="green">{url}</Text></Alert>
+        </Box>
+    );
 }
